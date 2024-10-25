5d8a9034e5470fcb41c502930b44b792953fa267
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-10-25 18:21:56 +0200

    add solution

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -6,16 +6,16 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class Main {
-  public static void main(String[] args) {
-      try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit");
-           EntityManager em = emf.createEntityManager()) {
+    public static void main(String[] args) {
+        try (EntityManagerFactory emf = Persistence.createEntityManagerFactory("artclass_persistence_unit");
+             EntityManager em = emf.createEntityManager()) {
 
-              em.getTransaction().begin();
-              Student student = new Student();
-              student.setName("John3");
+            em.getTransaction().begin();
+            Student student = new Student();
+            student.setName("John3");
 
-              em.persist(student);
-              em.getTransaction().commit();
-      }
-  }
+            em.persist(student);
+            em.getTransaction().commit();
+        }
+    }
 }
