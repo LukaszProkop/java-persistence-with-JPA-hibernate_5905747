678b836d997649640a31e2b9f9c0d57f2e5d5264
Lukasz_Prokop <Lukasz_Prokop@epam.com>
2024-10-25 16:27:58 +0200

    add connection

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -1,14 +1,20 @@
 package com.mycompany.app;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Main {
   public static void main(String[] args) {
-    /*
-     * EntityManagerFactory emf = new HibernatePersistenceProvider()
-     * .createContainerEntityManagerFactory(new DemoPersistenceUnitInfo(), new
-     * HashMap<>());
-     * 
-     * EntityManager em = emf.createEntityManager(); // Represent the persistence
-     * context
-     */
+//    /*
+//     * EntityManagerFactory emf = new HibernatePersistenceProvider()
+//     * .createContainerEntityManagerFactory(new DemoPersistenceUnitInfo(), new
+//     * HashMap<>());
+//     *
+//     * EntityManager em = emf.createEntityManager(); // Represent the persistence
+//     * context
+//     */
+    EntityManagerFactory factory = Persistence.createEntityManagerFactory("library_persistence_unit");
+    EntityManager manager = factory.createEntityManager();
   }
 }
